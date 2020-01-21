4db1163f92fe6b1cdc557941c5c3d24d4cf529f7
Makayla Reese <makaylareese15@gmail.com>
2020-01-20 18:26:27 -0800

    Created ModelBinding Animal Controller and View

---
+++ AnimalBears/Controllers/MBAnimalController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalBears.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalBears.Controllers
+{
+    public class MBAnimalController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Animal a)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
