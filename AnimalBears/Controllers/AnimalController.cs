a1afbca712ebdd39a0f62fb2a91eeb21704995f2
Makayla Reese <makaylareese15@gmail.com>
2020-01-20 18:25:33 -0800

    Created Animal controller and View without using Model Binding

---
+++ AnimalBears/Controllers/AnimalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalBears.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalBears.Controllers
+{
+    public class AnimalController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection data)
+        {
+            Animal a = new Animal();
+            a.AnimalName = data["animal-name"];
+            a.Name = data["name"];
+            a.Color = data["color"];
+
+            ViewData["Message"] = "Stuffed Animal Created";
+
+            return View();
+        }
+    }
+}
