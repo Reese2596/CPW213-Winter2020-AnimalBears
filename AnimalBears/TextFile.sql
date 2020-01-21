f5083085d1ecb1d8ea2a5743cc348f3a3060a799
Makayla Reese <makaylareese15@gmail.com>
2020-01-20 18:46:03 -0800

    Created AnimalDB and created connection to Project

---
+++ AnimalBears/TextFile.sql
@@ -0,0 +1,11 @@
+﻿Create Database AnimalDB
+Go
+
+Create Table Animal
+(
+	AnimalID		Int			Primary Key		Identity
+	, Name			varchar(20)	Not Null
+	, AnimalNAme	varchar(30)	Not Null
+	, Color			varchar(20)	Not Null
+)
+Go
